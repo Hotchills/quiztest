cbaf1a2db152ec41c86a8a7502da69b16cb23881
X <33039497+x@users.noreply.github.com>
2018-12-07 13:56:10 +0100

    safe

--- MyProject0.1/app/Http/Controllers/PlayController.php
+++ MyProject0.1/app/Http/Controllers/PlayController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 
 class PlayController extends Controller
 {
+        function __construct() {
+        $this->middleware('auth', ['except' => ['index', 'saveplay']]);
+    }
+    
     /**
      * Display a listing of the resource.
      *
