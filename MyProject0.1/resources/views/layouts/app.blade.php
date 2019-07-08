add0c605c16ee4ec896cd730f930b222c2df840e
mmorin2 <42168823+mmorin2@users.noreply.github.com>
2019-07-08 09:39:35 +0200

    [

--- MyProject0.1/resources/views/layouts/app.blade.php
+++ MyProject0.1/resources/views/layouts/app.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -178,80 +177,75 @@
         </style>
     </head>
     <body>
-        <div id="app">
-            <nav class="navbar navbar-expand-md navbar-light navbar-laravel"Style="padding-left:15%;padding-right:15%;background:#00a63f;box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);">
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel"Style="padding-left:15%;padding-right:15%;background:#00a63f;box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.5);">
 
-                    <a href="{{ url('/') }}">     <img  src="/images/Mainpage/logo-smal.png" height="50px"/>
-                         </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+        <a href="{{ url('/') }}">     <img  src="/images/Mainpage/logo-smal.png" height="50px"/>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto navbar-left">
-                        </ul>
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto navbar-right">
-                            <!-- Authentication Links -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto navbar-left">
+            </ul>
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto navbar-right">
+                <!-- Authentication Links -->
 
-                            @if(0)  
-                            <a  href="{{ route('login') }}"><strong>{{ __('Login') }}</strong></a>
-                            @endif
+                @if(0)
+                    <a  href="{{ route('login') }}"><strong>{{ __('Login') }}</strong></a>
+                @endif
 
-                            @guest
-                            <!-- No menu for not logged in users -->
-                            @else
-                            <!-- menu for logged in users -->
-                            <li class="nav-item dropdown">
-                                <a Style='padding-left: 18px;padding-right: 18px;' id="navbarDropdown" class="nav-link dropdown-toggle btn btn-default" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    <strong>{{ ucfirst(Auth::user()->name) }} </strong>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                @guest
+                <!-- No menu for not logged in users -->
+                @else
+                <!-- menu for logged in users -->
+                    <li class="nav-item dropdown">
+                        <a Style='padding-left: 18px;padding-right: 18px;' id="navbarDropdown" class="nav-link dropdown-toggle btn btn-default" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <strong>{{ ucfirst(Auth::user()->name) }} </strong>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
 
-                                    <a class="dropdown-item" href="/home" >Home</a>
-                                    <a class="dropdown-item" href="/CreateGuestUser" >Create User</a>
-                                    <a class="dropdown-item" href="/LoginUser" >Login Page with code</a>
-                                    <a class="dropdown-item" href="/{{Auth::user()->location}}/ListUser"  >Assign User to Quiz</a>
-                                    <a class="dropdown-item" href="/CreateQuiz" >Create quiz</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}  
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                            @endguest
-                        </ul>
-
-            </nav>
-
-            <main class="py-4">
-                <div class="center">
-                    <div class="col-md-8">
-                        @if (session()->has('message'))
-                        <div class="alert alert-success">
-                            {{ session()->get('message') }}
+                            <a class="dropdown-item" href="/home" >Home</a>
+                            <a class="dropdown-item" href="/CreateGuestUser" >Create User</a>
+                            <a class="dropdown-item" href="/LoginUser" >Login Page with code</a>
+                            <a class="dropdown-item" href="/{{Auth::user()->location}}/ListUser"  >Assign User to Quiz</a>
+                            <a class="dropdown-item" href="/CreateQuiz" >Create quiz</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
                         </div>
-                        @endif
+                    </li>
+                @endguest
+            </ul>
 
-                        @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        @endif
-                        
-                        @yield('content')
+    </nav>
 
+    <main class="py-4">
+        <div class="center">
+            <div class="col-md-8">
+                @if (session()->has('message'))
+                    <div class="alert alert-success">
+                        {{ session()->get('message') }}
                     </div>
-                </div>
-            </main>
+                @endif
+
+                @if ($errors->any())
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+
+                @yield('content')
+
+            </div>
         </div>
     </body>
 </html>
